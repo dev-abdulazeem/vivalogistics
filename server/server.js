914fc6ca7840e291863c75ec33f78e8b6e3ab7ab
Bamidele Azeem <lilmaya490@gmail.com>
2026-06-21 17:32:52 -0400

    frontend link added to server

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ const limiter = rateLimit({
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
+  'https://vivalogistics.vercel.app',
   process.env.FRONTEND_URL,
 ].filter(Boolean); // Remove undefined
 
